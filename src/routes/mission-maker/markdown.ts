9c2b548298b333865a3cc95952f72c9ac950bf62
Auroratide <tf.auroratide@gmail.com>
2026-06-01 18:22:03 -0500

    Special lists

--- src/routes/mission-maker/markdown.ts
+++ src/routes/mission-maker/markdown.ts
@@ -1,4 +1,5 @@
 import MarkdownIt from "markdown-it"
+import type { StateCore } from "markdown-it/index.js"
 
 let currentImageMap: Map<string, string> = new Map()
 
@@ -26,6 +27,50 @@ if (defaultImageRenderer) {
 		return defaultImageRenderer(tokens, idx, options, env, self)
 	}
 }
+
+const createSpecialList =
+	(name: string, symbol: string) => (state: StateCore) => {
+		const { Token, tokens } = state
+
+		for (let i = 0; i < tokens.length; i++) {
+			if (tokens[i].type !== "inline") continue
+
+			const lines = tokens[i].content.split("\n")
+			if (!lines.every((line) => line.startsWith(`${symbol} `))) continue
+
+			const replacement = []
+
+			const listOpen = new Token("bullet_list_open", "ul", 1)
+			listOpen.attrSet("class", name)
+			replacement.push(listOpen)
+
+			for (const item of lines.map((l) => l.slice(3))) {
+				replacement.push(new Token("list_item_open", "li", 1))
+				const inline = new Token("inline", "", 0)
+				inline.content = item
+				inline.children = []
+				replacement.push(inline)
+				replacement.push(new Token("list_item_close", "li", -1))
+			}
+
+			replacement.push(new Token("bullet_list_close", "ul", -1))
+
+			// paragraph_open is at i-1, inline at i, paragraph_close at i+1
+			tokens.splice(i - 1, 3, ...replacement)
+			i = i - 1 + replacement.length - 1
+		}
+	}
+
+md.core.ruler.before(
+	"inline",
+	"question-list",
+	createSpecialList("question-list", "?-"),
+)
+md.core.ruler.before(
+	"inline",
+	"exclaim-list",
+	createSpecialList("exclaim-list", "!-"),
+)
 
 export function renderToPages(
 	content: string,
